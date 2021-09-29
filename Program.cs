ca0c490f3607896123f3e30b1f86c4c77974a611
Арсений Сендецкий <ars_121229@mail.ru>
2021-09-30 01:53:02 +0800

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace gitlearn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
