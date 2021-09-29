adf8b632e38903480c3f9df3056772f5d695b02c
Арсений Сендецкий <ars_121229@mail.ru>
2021-09-30 02:12:54 +0800

    Добавили логику

---
+++ Logic.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace gitlearn
+{
+    class Logic
+    {
+        public int PowerOf2(int a)
+        {
+            return a * a;
+        }
+    }
+}
