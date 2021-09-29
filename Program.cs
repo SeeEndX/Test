a70100884f495806c91efed9c773c4a1f175ca48
Арсений Сендецкий <ars_121229@mail.ru>
2021-09-30 02:01:03 +0800

    добавил запрос числа и вывел квадрат

--- Program.cs
+++ Program.cs
@@ -6,6 +6,9 @@ namespace gitlearn
     {
         static void Main(string[] args)
         {
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine($"{a} * {a} = {a * a}");
+
             Console.WriteLine("Hello World!");
         }
     }
